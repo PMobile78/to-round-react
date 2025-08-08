3253ac0d211147968392c79676169aaf93839f95
Dmytro Tretiak <pmobile78@gmail.com>
2025-08-08 17:05:48 +0300

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,9 @@
 self.addEventListener('notificationclick', function (event) {
     event.notification.close();
     // Можно добавить переход по ссылке, если нужно
-}); 
+});
+
+// Простой fetch-обработчик (pass-through), чтобы удовлетворить критерии PWA
+self.addEventListener('fetch', function () {
+    // Ничего не кэшируем, просто даём браузеру обрабатывать запросы
+});
